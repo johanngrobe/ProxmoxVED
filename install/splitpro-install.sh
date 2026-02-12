ea748c56a72396241e262d4c5d21cc70ef7e8cdb
Johann Grobe <j.grobe@namowo.de>
2026-02-12 11:57:41 +0100

    fix pg_cron

--- install/splitpro-install.sh
+++ install/splitpro-install.sh
@@ -19,19 +19,20 @@ $STD apt install -y \
 msg_ok "Installed Dependencies"
 
 NODE_VERSION="22" NODE_MODULE="pnpm" setup_nodejs
-PG_VERSION="16" PG_MODULES="cron" setup_postgresql
+PG_VERSION="17" setup_postgresql
 
-msg_info "Configuring pg_cron Extension"
+msg_info "Installing pg_cron Extension"
+$STD apt install -y postgresql-${PG_VERSION}-cron
 sudo -u postgres psql -c "ALTER SYSTEM SET cron.database_name = 'postgres'"
 sudo -u postgres psql -c "ALTER SYSTEM SET cron.timezone = 'UTC'"
 systemctl restart postgresql
 sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS pg_cron"
-msg_ok "Configured pg_cron Extension"
+msg_ok "Installed pg_cron Extension"
 
 PG_DB_NAME="splitpro" PG_DB_USER="splitpro" setup_postgresql_db
 
 get_lxc_ip
-fetch_and_deploy_gh_release "splitpro" "oss-apps/split-pro" "tarball" "latest" "/opt/splitpro"
+fetch_and_deploy_gh_release "split-pro" "oss-apps/split-pro" "tarball" "latest" "/opt/splitpro"
 
 msg_info "Installing Dependencies"
 cd /opt/splitpro
