9057576ee5c54a47a49c85c0c03f5ee0cfa513e7
Johann Grobe <j.grobe@namowo.de>
2026-02-12 18:24:40 +0100

    fix .env

--- install/split-pro-install.sh
+++ install/split-pro-install.sh
@@ -25,8 +25,8 @@ msg_info "Installing pg_cron Extension"
 $STD apt install -y postgresql-17-cron
 sed -i "/^#shared_preload_libraries/s/^#//" /etc/postgresql/17/main/postgresql.conf
 sed -i "/^shared_preload_libraries/s/''/pg_cron/" /etc/postgresql/17/main/postgresql.conf
-sudo -u postgres psql -c "ALTER SYSTEM SET cron.database_name = 'postgres'"
-sudo -u postgres psql -c "ALTER SYSTEM SET cron.timezone = 'UTC'"
+$STD sudo -u postgres psql -c "ALTER SYSTEM SET cron.database_name = 'postgres'"
+$STD sudo -u postgres psql -c "ALTER SYSTEM SET cron.timezone = 'UTC'"
 systemctl restart postgresql
 sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS pg_cron"
 msg_ok "Installed pg_cron Extension"
@@ -42,11 +42,6 @@ cd /opt/split-pro
 $STD pnpm install --frozen-lockfile
 msg_ok "Installed Dependencies"
 
-msg_info "Building Application"
-cd /opt/split-pro
-$STD pnpm build
-msg_ok "Built Application"
-
 msg_info "Configuring Split Pro"
 cd /opt/split-pro
 mkdir -p /opt/split-pro_data/uploads
@@ -56,13 +51,22 @@ cat <<EOF >/opt/split-pro/.env
 DATABASE_URL=postgresql://${PG_DB_USER}:${PG_DB_PASS}@localhost:5432/${PG_DB_NAME}
 NEXTAUTH_SECRET=${NEXTAUTH_SECRET}
 NEXTAUTH_URL=http://${LOCAL_IP}:3000
+NEXTAUTH_URL_INTERNAL=http://localhost:3000
 DEFAULT_HOMEPAGE=/home
 CLEAR_CACHE_CRON_RULE=0 2 * * 0
 CACHE_RETENTION_INTERVAL=2 days
 EOF
+msg_ok "Configured Split Pro"
+
+msg_info "Building Application"
+cd /opt/split-pro
+$STD pnpm build
+msg_ok "Built Application"
+
+msg_info "Running Database Migrations"
 cd /opt/split-pro
 $STD pnpm exec prisma migrate deploy
-msg_ok "Configured Split Pro"
+msg_ok "Database Migrations Complete"
 
 msg_info "Creating Service"
 cat <<EOF >/etc/systemd/system/split-pro.service
