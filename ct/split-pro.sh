72b5a9a66b0fc7f5e59b95a40ceb33831cc2b70c
Johann Grobe <j.grobe@namowo.de>
2026-02-12 19:13:19 +0100

    fix pg_cron error

--- ct/split-pro.sh
+++ ct/split-pro.sh
@@ -8,7 +8,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/johanngrobe/ProxmoxVED/add
 APP="Split-Pro"
 var_tags="${var_tags:-finance;expense-sharing}"
 var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-2048}"
+var_ram="${var_ram:-4096}"
 var_disk="${var_disk:-6}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
--- install/split-pro-install.sh
+++ install/split-pro-install.sh
@@ -25,13 +25,16 @@ msg_info "Installing pg_cron Extension"
 $STD apt install -y postgresql-17-cron
 sed -i "/^#shared_preload_libraries/s/^#//" /etc/postgresql/17/main/postgresql.conf
 sed -i "/^shared_preload_libraries/s/''/pg_cron/" /etc/postgresql/17/main/postgresql.conf
-$STD sudo -u postgres psql -c "ALTER SYSTEM SET cron.database_name = 'postgres'"
+$STD sudo -u postgres psql -c "ALTER SYSTEM SET cron.database_name = 'splitpro'"
 $STD sudo -u postgres psql -c "ALTER SYSTEM SET cron.timezone = 'UTC'"
 systemctl restart postgresql
-$STD sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS pg_cron"
 msg_ok "Installed pg_cron Extension"
 
 PG_DB_NAME="splitpro" PG_DB_USER="splitpro" setup_postgresql_db
+
+msg_info "Enabling pg_cron in splitpro Database"
+$STD sudo -u postgres psql -d splitpro -c "CREATE EXTENSION IF NOT EXISTS pg_cron"
+msg_ok "Enabled pg_cron in splitpro Database"
 
 get_lxc_ip
 
